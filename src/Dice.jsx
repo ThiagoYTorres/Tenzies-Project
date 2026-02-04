4012fc9f58a04f078a9430abd06560aca92eb5a7
Thiago Yashimura Torres <thiagoyashimura03@gmail.com>
2026-02-04 10:21:19 -0300

    criação de um mapa de imagens

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,11 +1,24 @@
-import {useState} from 'react'
+import dice1 from "./imgs/dice1.png"
+import dice2 from "./imgs/dice2.png"
+import dice3 from "./imgs/dice3.png"
+import dice4 from "./imgs/dice4.png"
+import dice5 from "./imgs/dice5.png"
+import dice6 from "./imgs/dice6.png"
+
+const dadosImg = {
+  1: dice1,
+  2: dice2,
+  3: dice3,
+  4: dice4,
+  5: dice5,
+  6: dice6,
+}
 
 export default function Dice(props) {
-  const diceImg = `./src/imgs/dice${props.num}.png`
   return (
     <div className={props.select ? 'dice selected' : 'dice'}
     onClick={() => props.selecionar(props.id)}>
-      <img src={diceImg} draggable="false"/>
+      <img src={dadosImg[props.num]} draggable="false"/>
       </div>
   )
 }
